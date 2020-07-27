00a9ba2e0ccb545fa760bdeeba12f7b3ad615aaf
Sol <saulsallah1@gmail.com>
2020-07-27 16:49:14 -0400

    got prompts to repeat until user is done & create html w info

--- Develop/app.js
+++ Develop/app.js
@@ -9,6 +9,135 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/Employee");
+
+const questions = ["What's this employee's role? ",
+    "What's this employee's name? ",
+    "What's this employee's id? ",
+    "What's this employee's email?",
+    "What's this employee's school?",
+    "What's this employee's github username? ",
+    "What's this employee's office number? ",
+    "Would you like to add another employee? ",
+    "Would you like to add another employee? "
+];
+
+const employees = [];
+const newEmployees = () => {
+    var newManager = new Manager();
+    var newEngineer = new Engineer();
+    var newIntern = new Intern();
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: questions[0],
+                name: "role",
+                choices: ["Manager", "Engineer", "Intern"]
+            },
+            {
+                type: "input",
+                message: questions[1],
+                name: "name"
+            },
+            {
+                type: "input",
+                message: questions[2],
+                name: "id"
+            },
+            {
+                type: "input",
+                message: questions[3],
+                name: "email"
+            }
+
+        ]).then(data => {
+
+            if (data.role === "Manager") {
+                var newGuy = new Manager(data.name, data.id, data.email, "")
+
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: questions[6],
+                            name: "office"
+                        },
+                        {
+                            type: "list",
+                            message: questions[8],
+                            name: "add",
+                            choices: ["yes", "no"]
+                        }
+                    ]).then(dataTwo => {
+                        newGuy.officeNumber = dataTwo.office
+                        newManager = newGuy;
+                        employees.push(newManager);
+                        // console.log(employees);
+                        if (dataTwo.add === "yes") {
+                            newEmployees();
+                        } else {
+                            render(employees);
+                        }
+                    });
+            } else if (data.role === "Engineer") {
+                var newGuy = new Engineer(data.name, data.id, data.email, "")
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: questions[5],
+                            name: "github"
+                        },
+                        {
+                            type: "list",
+                            message: questions[8],
+                            name: "add",
+                            choices: ["yes", "no"]
+                        }
+                    ]).then(dataTwo => {
+                        newGuy.github = dataTwo.github
+                        newEngineer = newGuy;
+                        employees.push(newEngineer);
+                        // console.log(employees);
+                        if (dataTwo.add === "yes") {
+                            newEmployees();
+                        } else {
+                            render(employees);
+                        }
+                    });
+            } else {
+                var newGuy = new Intern(data.name, data.id, data.email, "")
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: questions[4],
+                            name: "school"
+                        },
+                        {
+                            type: "list",
+                            message: questions[8],
+                            name: "add",
+                            choices: ["yes", "no"]
+                        }
+                    ]).then(dataTwo => {
+                        newGuy.school = dataTwo.school;
+                        newIntern = newGuy;
+                        employees.push(newIntern);
+                        // console.log(employees);
+                        if (dataTwo.add === "yes") {
+                            newEmployees();
+                        } else {
+                            render(employees);
+                        }
+                    });
+            }
+        })
+}
+
+newEmployees();
+
 
 
 // Write code to use inquirer to gather information about the development team members,
